2ef404968b62d0219c3dcae4a61d44412e2f50e5
sreepriya <sreepriyas@ikm.gov.in>
2023-04-12 09:27:56 +0530

    added new project

---
+++ src/main/java/com/ikm/ksmptxbasicservice/KsmPtxBasicServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ikm.ksmptxbasicservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsmPtxBasicServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsmPtxBasicServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ikm/ksmptxbasicservice/KsmPtxBasicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikm.ksmptxbasicservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsmPtxBasicServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
