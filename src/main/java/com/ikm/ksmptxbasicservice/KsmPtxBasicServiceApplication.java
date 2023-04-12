737641e3fc096a21d7753e1a0aae5ade60461247
sreepriya <sreepriyas@ikm.gov.in>
2023-04-12 09:30:41 +0530

    added a sysout

--- src/main/java/com/ikm/ksmptxbasicservice/KsmPtxBasicServiceApplication.java
+++ src/main/java/com/ikm/ksmptxbasicservice/KsmPtxBasicServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KsmPtxBasicServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hi");
 		SpringApplication.run(KsmPtxBasicServiceApplication.class, args);
 	}
 
